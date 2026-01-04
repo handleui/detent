74a3ae8be7edfd14d6d9d6ed14258023cf6e179d
handleui <rodrigo@handleui.com>
2026-01-04 16:49:03 -0600

    chore: old web

--- apps/web/app/install/route.ts
+++
@@ -1,8 +0,0 @@
-import { generateInstallScript } from "../../lib/install-script";
-
-export const GET = () =>
-  new Response(generateInstallScript(), {
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-    },
-  });
